e4c9a48853d2a3f0beaa21cbf4327137fe3b496b
Iván Lewin <57846638+ivanlewin@users.noreply.github.com>
2022-02-17 02:18:10 -0300

    06-01

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,29 +1,32 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 function UsernameForm({ onSubmitUsername }) {
 
+  const inputRef = React.createRef();
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSubmitUsername(event.target.elements[0].value);
-  }
+    if (inputRef.current === null) return;
+    onSubmitUsername(inputRef.current.value);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='username-input'>Username:</label>
-        <input type="text" id='username-input' />
+        <input type="text" id='username-input' ref={inputRef} />
       </div>
       <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
 function App() {
-  const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  const onSubmitUsername = username => alert(`You entered: ${username}`);
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />;
 }
 
-export default App
+export default App;

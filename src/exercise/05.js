c4d9cc6286d0f03bf34a91b8e7ca80c6067031fd
Iván Lewin <57846638+ivanlewin@users.noreply.github.com>
2022-02-17 02:10:08 -0300

    05-02

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,13 +4,22 @@
 import * as React from 'react';
 import '../box-styles.css';
 
-const Box = ({ className = '', style = {}, children }) => (
-  <div className={`box ${className}`} style={{ fontStyle: 'italic', ...style }}>{children}</div>
-);
+const Box = ({ size, style = {}, children, ...props }) => {
+  const className = size ? `box--${size}` : '';
+  return (
+    <div
+      className={`box ${className}`}
+      style={{ fontStyle: 'italic', ...style }}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
 
-const smallBox = <Box className='box--small' style={{ backgroundColor: 'lightblue' }}>small lightblue box</Box>;
-const mediumBox = <Box className='box--medium' style={{ backgroundColor: 'pink' }}>medium pink box</Box>;
-const largeBox = <Box className='box--large' style={{ backgroundColor: 'orange' }}>large orange box</Box>;
+const smallBox = <Box size='small' style={{ backgroundColor: 'lightblue' }}>small lightblue box</Box>;
+const mediumBox = <Box size='medium' style={{ backgroundColor: 'pink' }}>medium pink box</Box>;
+const largeBox = <Box size='large' style={{ backgroundColor: 'orange' }}>large orange box</Box>;
 
 function App() {
   return (
